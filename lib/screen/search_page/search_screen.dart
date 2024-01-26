9818e754c4c88bdef36afcc96dfbc9a7f7896633
iloveuhyeon <pyh1111802@naver.com>
2024-01-26 12:18:15 +0900

    make search sample

--- lib/screen/search_page/search_screen.dart
+++ lib/screen/search_page/search_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({Key? key}) : super(key: key);
 
   @override
   State<SearchScreen> createState() => _SearchScreenState();
@@ -9,62 +9,58 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   @override
-  late TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              Row(
-                children: [
-                  TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                        labelText: '아무거나 입력하세요',
-                        helperText: '공백, 글자, 숫자, 특수문자가 가능해요',
-                        hintText: '???', //글자를 입력하면 사라진다.
-                        icon: Icon(Icons.android),
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.all(3)),
-                    onSubmitted: (String value) async {
-                      await showDialog<void>(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Thanks!'),
-                            content: Text(
-                                'You typed "$value", which has length ${value.characters.length}.'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("정치 뉴스");
+                  },
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xff366dff),
+                        ),
+                        height: 30,
+                        width: 30,
+                        child: const Icon(
+                          Icons.search_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 17,
+                      ),
+                      const Text(
+                        "정치 뉴스",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              )
-            ],
+                ),
+                IconButton(
+                  icon: const Icon(Icons.cancel_outlined),
+                  color: const Color(0xff366dff),
+                  iconSize: 30,
+                  splashRadius: 20,
+                  onPressed: () {
+                    print("delete");
+                  },
+                )
+              ],
+            ),
           )
         ],
       ),
